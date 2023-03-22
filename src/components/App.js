206042fdb5783eb11e78f8eff1af35e1f5b45298
julien382 <julhermain@gmail.com>
2023-03-23 00:56:06 +0100

    tri

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import Home from '../pages/Home/Home';
+
+import '../styles/reset.css';
+import '../styles/index.css';
+
+function App() {
+
+
+  return (
+    <div id="app">
+
+      <Home />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div className='home'>
+            
+            <h1>test</h1>
+            
+        </div>
+    )
+}
+
+export default Home
