1a617a0447a39e77e66e47cbbf3262d01c19c9a9
julien382 <julhermain@gmail.com>
2023-03-23 02:41:30 +0100

    css

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -55,48 +55,53 @@ const Home = () => {
             <div className="container">
                 <a href="employee-list.html">View Current Employees</a>
                 <h2>Create Employee</h2>
-                <form action="#" id="create-employee">
-                <label htmlFor="first-name">First Name</label>
-                <input type="text" id="first-name" />
 
-                <label htmlFor="last-name">Last Name</label>
-                <input type="text" id="last-name" />
+                <form className='form' action="#" id="create-employee">
+                    <div className='row'>
+                        <div className='partName'>
+                            <label htmlFor="first-name">First Name</label>
+                            <input type="text" id="first-name" />
 
-                <label htmlFor="date-of-birth">Date of Birth</label>
-                <input id="date-of-birth" type="text" />
+                            <label htmlFor="last-name">Last Name</label>
+                            <input type="text" id="last-name" />
 
-                <label htmlFor="start-date">Start Date</label>
-                <input id="start-date" type="text" />
+                            <label htmlFor="date-of-birth">Date of Birth</label>
+                            <input id="date-of-birth" type="text" />
 
-                <fieldset className="address">
-                    <legend>Address</legend>
+                            <label htmlFor="start-date">Start Date</label>
+                            <input id="start-date" type="text" />
+                        </div>
+                        <div className='partAddress'>
+                            <label htmlFor="street">Street</label>
+                            <input id="street" type="text" />
 
-                    <label htmlFor="street">Street</label>
-                    <input id="street" type="text" />
+                            <label htmlFor="city">City</label>
+                            <input id="city" type="text" />
 
-                    <label htmlFor="city">City</label>
-                    <input id="city" type="text" />
+                            <label htmlFor="state">State</label>
+                            <select name="state" id="state" value={selectedState} onChange={handleStateChange}>
+                                <option value="">Select a state</option>
+                                {states().map((state) => (
+                                    <option key={state.abbreviation} value={state.abbreviation}>{state.name}</option>
+                                ))}
+                            </select>
 
-                    <label htmlFor="state">State</label>
-                    <select name="state" id="state" value={selectedState} onChange={handleStateChange}>
-                        <option value="">Select a state</option>
-                        {states().map((state) => (
-                            <option key={state.abbreviation} value={state.abbreviation}>{state.name}</option>
-                        ))}
-                    </select>
+                            <label htmlFor="zip-code">Zip Code</label>
+                            <input id="zip-code" type="number" />
+                        </div>
+                    </div>
+                    <div className='partDepartment'>
+                        <label htmlFor="department">Department</label>
+                        <select name="department" id="department">
+                            <option>Sales</option>
+                            <option>Marketing</option>
+                            <option>Engineering</option>
+                            <option>Human Resources</option>
+                            <option>Legal</option>
+                        </select>
+                    </div>
 
-                    <label htmlFor="zip-code">Zip Code</label>
-                    <input id="zip-code" type="number" />
-                </fieldset>
 
-                <label htmlFor="department">Department</label>
-                <select name="department" id="department">
-                    <option>Sales</option>
-                    <option>Marketing</option>
-                    <option>Engineering</option>
-                    <option>Human Resources</option>
-                    <option>Legal</option>
-                </select>
                 </form>
 
                 <button className='save' onClick={saveEmployee}>Save</button>
