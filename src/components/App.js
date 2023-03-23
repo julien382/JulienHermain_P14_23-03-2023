7299bf7ce4599c129a5a9c529b3a0455e71d2c0e
julien382 <julhermain@gmail.com>
2023-03-23 02:00:32 +0100

    tri

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from '../pages/Home/Home';
 
-
 //import '../styles/reset.css';
 import '../styles/index.css';
 
@@ -11,7 +10,6 @@ function App() {
   return (
     <div id="app">
       <Routes>
-        {/*<Route path="/employees" element={<CurrentEmployees />}></Route>*/}
         <Route path="/" element={<Home />}></Route>
       </Routes>
 
