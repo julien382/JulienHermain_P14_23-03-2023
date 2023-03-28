7d592357e43b3bd87e29db933bc7898f28ee0b2c
julien382 <julhermain@gmail.com>
2023-03-28 16:41:34 +0200

    test modal effet fondu

--- src/pages/Modal/Modal.jsx
+++ src/pages/Modal/Modal.jsx
@@ -5,25 +5,31 @@ const Modal = ({
     setIsOpen, // action
     isOpen, // état (ouvert/fermé)
     closingCross = true, // display cross
-    editClose = 'X' // modifier le closing button
+    editClose = 'X', // modifier le closing button
+    fadeDuration = null, // durée de l'effet de fondu (en millisecondes)
+    fadeDelay = null // ajoute un delay au fondu
 }) => {
     // fermeture de la modal
     const handleClose = () => {
         setIsOpen(false);
+    };
+    // transition de fondu
+    const transitionStyle = {
+        transition: `opacity ${fadeDuration}ms ease-in-out ${fadeDelay}ms`,
+        //opacity: isOpen ? 1 : 0
     };
 
     if(!isOpen) return null
     return (
         <div className="modal-container">
             <div className="shadow" onClick={()=> setIsOpen(false)}></div>
-            <div className="modal">
+            <div className={`modal`} style={transitionStyle}>
                 <div className='inModal'>
                     {closingCross && <span className="modal-close" onClick={handleClose}>{editClose}</span>}
                     {children}
                 </div>
             </div>
         </div>
-
     )
 }
 
